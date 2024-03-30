712602b5bb7f32d506016f68018ca7ee9d4b4f1a
Beata <beata@jasinski.com>
2024-03-30 14:16:27 -0700

    add todo package

--- main.go
+++ main.go
@@ -7,10 +7,19 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput(("Todo text: "))
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -18,6 +27,16 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	todo.Display()
+	err = todo.Save()
+
+	if err != nil {
+		fmt.Println("Saving the todo failed")
+		return
+	}
+
+	fmt.Println("Saving the todo succeeded!")
 
 	userNote.Display()
 	err = userNote.Save()
@@ -30,6 +49,11 @@ func main() {
 	fmt.Println("Saving the node succeeded!")
 
 }
+
+// func getTodoData() string {
+// 	text := getUserInput("todo text: ")
+// 	return text
+// }
 
 func getNoteData() (string, string) {
 	title := getUserInput("Note title:")
