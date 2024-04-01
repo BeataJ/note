537e66debd83a8920c33b26f4ca3fd60747f6766
Beata <beata@jasinski.com>
2024-04-01 11:31:25 -0700

    add sevsr interface

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 	"example.com/note/todo"
 )
 
+type saver interface {
+	Save() error
+}
+
 func main() {
 	title, content := getNoteData()
 	todoText := getUserInput(("Todo text: "))
@@ -29,24 +33,18 @@ func main() {
 	}
 
 	todo.Display()
-	err = todo.Save()
+	err = saveData(todo)
 
 	if err != nil {
-		fmt.Println("Saving the todo failed")
 		return
 	}
-
-	fmt.Println("Saving the todo succeeded!")
 
 	userNote.Display()
-	err = userNote.Save()
+	err = saveData(userNote)
 
 	if err != nil {
-		fmt.Println("Saving the note failed")
 		return
 	}
-
-	fmt.Println("Saving the node succeeded!")
 
 }
 
@@ -54,6 +52,18 @@ func main() {
 // 	text := getUserInput("todo text: ")
 // 	return text
 // }
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed")
+		return err
+	}
+
+	fmt.Println("Saving the node succeeded!")
+	return nil
+}
 
 func getNoteData() (string, string) {
 	title := getUserInput("Note title:")
