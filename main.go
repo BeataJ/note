53b6713d2dac7d1cbbe8424e93264aca9f7a6659
Beata <beata@jasinski.com>
2024-04-10 18:22:18 -0700

    value interface

--- main.go
+++ main.go
@@ -61,14 +61,35 @@ func main() {
 }
 
 func printSomething(value interface{}) {
-	switch value.(type) {
-	case int:
-		fmt.Println("Intenger:", value)
-	case float64:
-		fmt.Println("Float:", value)
-	case string:
-		fmt.Println(value)
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
 	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+
+	srtingVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("String:", srtingVal)
+		return
+	}
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println(value)
+	// }
 
 }
 
